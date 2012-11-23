704ee158e22c0a0100d6867eb4e30dbcde6fbf73
André Moraes <andrebq@gmail.com>
2012-11-23 21:12:21 -0200

    Tests OK

--- enc/gob.go
+++ enc/gob.go
@@ -2,20 +2,21 @@ package enc
 
 import (
 	"encoding/gob"
+	"github.com/andrebq/assimp"
 	"io"
 )
 
 // Write the file using the gob format
-func GobWrite(w io.Writer, s *assimp.Scene) {
+func GobWrite(w io.Writer, s *assimp.Scene) error {
 	enc := gob.NewEncoder(w)
 	err := enc.Encode(*s)
 	return err
 }
 
 // Read a file written using GobWrite function
-func GobRead(r io.Reader) *assimp.Scene {
+func GobRead(r io.Reader) (*assimp.Scene, error) {
 	dec := gob.NewDecoder(r)
-	sc := &Scene{}
+	sc := &assimp.Scene{}
 	err := dec.Decode(sc)
 	return sc, err
 }
--- enc/gob_test.go
+++ enc/gob_test.go
@@ -1,27 +1,28 @@
 package enc
 
 import (
-	"testing"
 	"bytes"
 	"github.com/andrebq/assimp"
+	"reflect"
+	"testing"
 )
 
 // Test the encoding/decoding of gob files
 func randomScene() *assimp.Scene {
 	sc := &assimp.Scene{}
-	mesh := &assimpMesh{}
+	mesh := &assimp.Mesh{}
 	v := assimp.Vector3{1, 1, 1}
 	mesh.Vertices = make([]assimp.Vector3, 0)
 	mesh.Vertices = append(mesh.Vertices, v)
 	sc.AddMesh(mesh)
-	
+
 	return sc
 }
 
 // Test the gob encoding/decoding
 func TestGob(t *testing.T) {
 	sc := randomScene()
-	
+
 	buff := new(bytes.Buffer)
 	err := GobWrite(buff, sc)
 	if err != nil {
@@ -30,11 +31,12 @@ func TestGob(t *testing.T) {
 
 	buff = bytes.NewBuffer(buff.Bytes())
 
-	sc, err = GobRead(buff)
+	fromDisk, err := GobRead(buff)
 	if err != nil {
 		t.Fatalf("Error reading scene. %v", err)
 	}
-	if sc.Mesh[0].Vertices[0] != v {
-		t.Errorf("Expecting %v but got %v", v, sc.Mesh[0].Vertices[0])
+
+	if !reflect.DeepEqual(sc.Mesh[0].Vertices, fromDisk.Mesh[0].Vertices) {
+		t.Errorf("Vertex array is different")
 	}
 }
